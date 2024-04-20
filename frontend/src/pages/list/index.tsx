3f50ba616f6d0ad3df03a7795aafc27bda648ffd
宮本航平 <captain.marvel.mk@gmail.com>
2024-04-20 23:27:15 +0900

    リストページアクセス時にBEにリクエストを送る処理追加

--- frontend/src/pages/list/index.tsx
+++ frontend/src/pages/list/index.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
 import { Card } from "../../components/Card";
 
 export default function ListPage() {
+  useEffect(() => {
+    const getLists = async () => {
+      try {
+        const response = await fetch("http://localhost:8989/api/list");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // レスポンス処理
+      } catch (error) {
+        console.error("Failed to fetch:", error);
+      }
+    };
+    getLists();
+  }, []);
   return <Card />;
 }
