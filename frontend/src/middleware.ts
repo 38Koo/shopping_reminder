4c7a23df655559a0a3da46e9f4152ff47e2b56a7
宮本航平 <captain.marvel.mk@gmail.com>
2024-04-21 19:19:22 +0900

    認証実装 - listページにアクセスしてlistを返す

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   ignoredRoutes: ["/"],
+  afterAuth(auth, req, _) {
+    const signInPath = new URL("/", req.url);
+
+    if (!auth.sessionId && !auth.userId) {
+      return NextResponse.redirect(signInPath);
+    }
+  },
 });
 
 export const config = {
--- frontend/src/pages/list/index.tsx
+++ frontend/src/pages/list/index.tsx
@@ -1,15 +1,27 @@
 import { useEffect } from "react";
 import { Card } from "../../components/Card";
+import { currentUser, useAuth, useUser } from "@clerk/nextjs";
 
 export default function ListPage() {
+  const { getToken } = useAuth();
+
   useEffect(() => {
     const getLists = async () => {
       try {
-        const response = await fetch("http://localhost:8989/api/list");
+        const token = await getToken();
+
+        const response = await fetch("http://localhost:8989/api/list", {
+          headers: { Authorization: `Bearer ${token}` },
+          credentials: "include",
+        });
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         // レスポンス処理
+        const data = await response.json();
+
+        console.log(data);
       } catch (error) {
         console.error("Failed to fetch:", error);
       }
