475a6271028236d4db7b364c8179779f554e6731
宮本航平 <captain.marvel.mk@gmail.com>
2024-06-12 20:04:07 +0900

    FEからのリクエストが想定と異なっていたため、修正

--- frontend/src/components/Report/types/ReportFormtypes.ts
+++ frontend/src/components/Report/types/ReportFormtypes.ts
@@ -1,17 +1,29 @@
 import { z } from "zod";
 
-export type ReportFormType = {
-  report: z.infer<typeof reportFormSchema>[];
-};
+export type ReportFormType = z.infer<typeof reportFormSchemaArray>;
 
-const reportFormSchema = z.object({
-  PurchaseAmount: z
-    .preprocess((v) => Number(v), z.number({ message: "入力が不正です。" }))
-    .optional(),
-  PurchaseDate: z.date().optional(),
-  itemID: z.number(),
-});
+const reportFormSchema = z
+  .array(
+    z.object({
+      PurchaseAmount: z
+        .preprocess((v) => Number(v), z.number({ message: "入力が不正です。" }))
+        .optional(),
+      PurchaseDate: z.date().optional(),
+      itemID: z.number(),
+    })
+  )
+  .refine((data) => {
+    const validItems = data.filter(
+      (item) => item.PurchaseAmount && item.PurchaseDate
+    );
+    return validItems.length > 0;
+  })
+  .transform((data) =>
+    data.filter((item) => {
+      return item.PurchaseAmount && item.PurchaseDate;
+    })
+  );
 
 export const reportFormSchemaArray = z.object({
-  report: reportFormSchema.array(),
+  report: reportFormSchema,
 });
