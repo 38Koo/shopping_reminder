983ebe875a9b2a25b53b9db688ea1db242375477
宮本航平 <captain.marvel.mk@gmail.com>
2024-07-20 15:49:07 +0900

    item登録apiのurlが間違っていたので修正

--- frontend/src/components/Add/component/AddFormGroup/index.tsx
+++ frontend/src/components/Add/component/AddFormGroup/index.tsx
@@ -41,7 +41,7 @@ export const AddFormGroup = () => {
 
   return (
     <Form
-      action={`${process.env.NEXT_PUBLIC_API_SERVER_DOMAIN}/add/item`}
+      action={`${process.env.NEXT_PUBLIC_API_SERVER_DOMAIN}/api/add/item`}
       method="post"
       headers={{
         Authorization: `Bearer ${token}`,
