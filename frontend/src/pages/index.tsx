ce2f5c4dd6f466e5ee94bee313eadd635f5b8909
宮本航平 <captain.marvel.mk@gmail.com>
2024-07-21 19:28:35 +0900

    すでにログイン済みでsigninやsignupをしようとした際に、メッセージを表示させる。

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,11 @@
-import { Box, Button, HStack } from "@yamada-ui/react";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Box, Button, HStack, Link, Text } from "@yamada-ui/react";
+import { SignInButton, SignUpButton, useAuth } from "@clerk/nextjs";
+import { Icon } from "@yamada-ui/fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const { isSignedIn } = useAuth();
+
   return (
     <Box
       display="flex"
@@ -11,24 +15,56 @@ export default function Home() {
       justifyContent="center"
     >
       <Box
-        border="solid 1px red"
+        border="solid 5px orange"
         width="500px"
         height="500px"
         borderRadius="16px"
+        position="relative"
       >
         <HStack justifyContent="space-evenly" alignItems="center" height="100%">
           <SignUpButton mode="modal">
-            <Button p="md" rounded="4" bg="primary" color="white">
+            <Button
+              p="md"
+              rounded="4"
+              bg="primary"
+              color="white"
+              onClick={
+                isSignedIn
+                  ? () => alert("すでにサインアップしています。")
+                  : undefined
+              }
+            >
               Sign Up
             </Button>
           </SignUpButton>
-
           <SignInButton mode="modal">
-            <Button p="md" rounded="4" bg="primary" color="white">
+            <Button
+              p="md"
+              rounded="4"
+              bg="primary"
+              color="white"
+              onClick={
+                isSignedIn
+                  ? () => alert("すでにサインインしています。")
+                  : undefined
+              }
+            >
               Sign In
             </Button>
           </SignInButton>
         </HStack>
+        {isSignedIn && (
+          <Box position="absolute" top="58%" right="50px">
+            <Link href="/list" color="gray">
+              <HStack gap={1}>
+                <Icon icon={faChevronRight} size="2xl" />
+                <Text fontSize="20px" fontWeight="bold">
+                  一覧ページへ
+                </Text>
+              </HStack>
+            </Link>
+          </Box>
+        )}
       </Box>
     </Box>
   );
